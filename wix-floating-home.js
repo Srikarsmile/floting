8ddc22ab478c2657de2dcbbe977bc95fa825c9e7
Srikar_Reddy <srikarsmile10@gmail.com>
2026-05-15 07:14:31 +0530

    Update community hub locations

--- wix-floating-home.js
+++ wix-floating-home.js
@@ -13,7 +13,7 @@ class FloatingHome extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.assetBase = floatingHomeAssetBase;
-    this.version = '20260514-05';
+    this.version = '20260515-01';
     this.isolationTimer = 0;
     this.isolationObserver = null;
     this.layoutWatchdog = 0;
