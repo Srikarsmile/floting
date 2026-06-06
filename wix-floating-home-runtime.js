8cfc19f35ab7936f4d58aea3a652cf582f373a09
Srikar_Reddy <srikarsmile10@gmail.com>
2026-06-06 20:19:43 +0530

    Show Community in site header

--- wix-floating-home-runtime.js
+++ wix-floating-home-runtime.js
@@ -34,7 +34,7 @@ const floatingHomeAssetBase = (() => {
   return floatingHomeDefaultAssetBase;
 })();
 
-const floatingHomeCurrentBuild = String(floatingHomeRuntimeManifest.version || '20260606-03');
+const floatingHomeCurrentBuild = String(floatingHomeRuntimeManifest.version || '20260606-04');
 
 const floatingHomeImageAssetAliases = Object.freeze({
   'images/team-celestina.jpg': 'images/team-celestina-20260601.webp',
